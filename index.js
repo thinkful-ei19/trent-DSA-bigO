7c7f652c25bfd949893354d954112d7402600168
trentduncan <trent.dunc@gmail.com>
2018-05-15 11:52:15 -0700

    finished big O analysis of iterative solutions from yesterday

--- index.js
+++ index.js
@@ -144,5 +144,114 @@
 // Still O(n) because in the worst case scenario it has a single
 // non nested loop. 
 
+// Recursive Solutions from yesterday
+
+
+// Iterative Solutions from yesterday
+
+// function countSheepLoop(num){
+//     for(let i=num; i>0; i--){
+//         console.log(`counting sheeps ${i}`);
+//     }
+// }
+// countSheepLoop(10);
+
+// O(n) single non nested loop, scales linearly
+
+
+// function double_all(arr) {
+//     var ret = Array(arr.length);
+//     for (var i = 0; i < arr.length; ++i) {
+//         ret[i] = arr[i] * 2;
+//     }
+//     return ret;
+// }
+// let arr = [10,4,5,2,1];
+// console.log(double_all(arr));
+
+// O(n) single non nested loop, scales linearly
+
+
+// function reverse_tail(str) {
+//     var accumulator = "";
+//     while (str !== "") {
+//     	accumulator = str[0] + accumulator;
+//     	str = str.slice(1);
+//     }
+//     return accumulator;
+// }
+
+// O(n) single non nested loop, scales linearly. the longer the str
+// the more operations
+
+
+// function triangle(n) {
+//     var tot = 0;
+//     for (var i = 1; i <= n; ++i) {
+// 	    tot += n;
+//     }
+//     return tot;
+// }
+
+// O(n) single non nested loop, scales linearly
+
+
+// function split(str, sep) {
+//     var ret = [];
+//     while (true) {
+//         var idx = str.indexOf(sep);
+//         if (idx == -1) break;
+// 	ret.push(str.slice(0, idx))
+// 	str = str.slice(idx + sep.length);
+//     }
+//     ret.push(str);
+//     return ret;
+// }
+
+// O(n) single non nested loop, scales linearly, the longer the str
+// the more operations 
+
+
+// function convertToBinaryIter(num){
+//     var binary = '';
+//     while(num>0){
+//         let rem = Math.floor(num%2);
+//         binary = rem + binary;
+//         num = Math.floor(num/2);
+//     }
+//     return binary;
+
+
+// }
+// console.log(convertToBinaryIter(124)); //1111100
+
+// O(n) single non nested loop, scales linearly, the bigger the numeric
+// input the more operations necessary
+
+
+// function factorialIterative(number){
+//     let fact = 1;
+//     for (let i = 1; i <= number; i++){
+//         fact *= i;
+//     }
+//     return fact;
+//  }
+//  console.log(factorialIterative(5));
+
+// O(n) single non nested loop, scales linearly
+
+
+
+// function fibonacciIterative2(number){
+//     let [num1, num2] = [1,0];
+//     while(number-- > 0){
+//         [num1, num2] = [num2+num1, num1]
+//     }
+//     return num2;
+
+// }
+// console.log(fibonacciIterative2(3));
+
+// O(n) single non nested loop, scales linearly
 
 
